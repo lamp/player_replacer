b91832deaa8719470747b004cd0756fc0f2d051c
Matt <matthew.gradidge@gmail.com>
2011-11-02 10:57:25 +0000

    further changes

--- replacer.js
+++ replacer.js
@@ -12,22 +12,38 @@ PlayerReplacer.configure = function(options){
   replacer.seed_name = options.seed_name;
   replacer.dimensions = options.dimensions;
   replacer.replace();
-  $('.replaced_thumb').bind('click', replacer.switch_player);
+  $('.replaced_thumb').click(function(e){
+    e.preventDefault();
+    replacer.switch_player(this);
+  });
   return replacer;
 };
 
 PlayerReplacer.prototype = {
- replace : function(){
-   var scope = this;
+  presentations : {},
+  replace : function(){
+    var scope = this;
     $(this.selector).each(function(i, v){
       var id        = $(this).attr('data-id'),
-          seed_name = $(this).attr('data-seed-name');
-      $(this).append('<img src="http://' + seed_name + '.thumbnails.videojuicer.com/' + seed_name + '/presentations/' + id + '.jpg?maxwidth=' + scope.dimensions.width + '&maxheight=' + scope.dimensions.height + '" class="replaced_thumb" />')
+          seed_name = $(this).attr('data-seed-name'),
+          img       = '<img src="http://' + seed_name + '.thumbnails.videojuicer.com/' + seed_name + '/presentations/' + id + '.jpg?maxwidth=' + scope.dimensions.width + '&maxheight=' + scope.dimensions.height + '" class="replaced_thumb" />';
+
+      scope.presentations[id] = { 'seed_name' : seed_name, 'img' : img };
+      $(this).html(img)
     });
   },
-  
-  switch_player : function(){
-    console.log('wowowowowowo')
+
+  switch_player : function(element){
+    var parent    = $(element).parent(),
+        id        = parent.attr('data-id'),
+        seed_name = parent.attr('data-seed-name');
+    //replace all players with thumbnails
+    this.replace();
+    console.log(id);
+    if(typeof(this.presentations[id].embed_code) == 'undefined'){
+      
+    }else{
+      parent.html(this.presentations[id].embed_code);
+    }  
   }
-  
-}
+};
